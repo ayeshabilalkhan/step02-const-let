bd68555be103071faa29494d21f7e5271ba51f84
ayeshabilalkhan <ayeshabilalkhan7@gmail.com>
2024-07-18 11:20:45 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//Use const where variable values do not change
+// Example:
+var a = 10;
+var b = 20;
+var c = "best";
+console.log(b);
+// Example:
+var PI = 3.14;
+// Example:
+var newName = "Ayeshy";
+console.log(newName); //Output: Ayeshy
+// Let
+var x = 10;
+// console.log(x); //Output: 10
+x = 20;
+console.log(x); //Output: 20
---
+++ app.ts
@@ -0,0 +1,25 @@
+//Use const where variable values do not change
+            //  Const
+// Example:
+const a = 10;
+const b : number = 20;
+const c = "best";
+
+console.log(b);
+
+// Example:
+const PI = 3.14;
+
+// Example:
+const newName = "Ayeshy";
+console.log(newName); //Output: Ayeshy
+
+// Let
+let x = 10;
+console.log(x); //Output: 10
+x = 20;
+console.log(x); //Output: 20
+
+
+
+
